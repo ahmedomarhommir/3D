aa9996794ef4bd5e99df9df3bd37196f4abf975c
ahmedomarhommir <73411282+ahmedomarhommir@users.noreply.github.com>
2020-10-25 10:37:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-alert("Hello From Github JavaScript");
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
+var renderer = new THREE.WebGLRenderer({antialias: true});
+render.setClearColor("#E5E5E5");
+render.setSize(window.innerWidth, window.innerHeight);
+document.getElementById("MyDiv").appendChild("ahmed");
