d073ca9ed0dc6452b29acf0dc8601ec799df97a5
ahmedomarhommir <73411282+ahmedomarhommir@users.noreply.github.com>
2020-10-25 10:56:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/*
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
 var renderer = new THREE.WebGLRenderer({antialias: true});
 render.setClearColor("#E5E5E5");
 render.setSize(window.innerWidth, window.innerHeight);
+*/
 document.getElementById("MyDiv").innerHTML= "<h1>Hello Word</h1>";
